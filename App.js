53d0faacd47722b062632632b20336e6ec101e81
Gabriel.souza@ernetwork.com.br <Gabriel.souza@ernetwork.com.br>
2018-09-10 22:02:17 -0300

    Autocomplet maps

--- App.js
+++ App.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import {
   StyleSheet, Text, View, Image, Button, Platform, TextInput,
   TouchableOpacity,ImageBackground,
-  FlatList, Alert
+  FlatList, Alert,Image 
 } from "react-native"
 import { androidClientId } from "./superSecretKey"
 import { googleMapsApi } from "./superSecretKey"
 import Expo from "expo"
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import Geocoder from 'react-native-geocoding';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+
+const homePlace = { description: 'Home', geometry: { location: { lat: 48.8152937, lng: 2.4597668 } }};
+const workPlace = { description: 'Work', geometry: { location: { lat: 48.8496818, lng: 2.2940881 } }};
 
 export default class App extends React.Component {
   constructor(props) {
@@ -163,6 +167,7 @@ const LoggedInPage = props => {
               color="#841584"
               accessibilityLabel="Proxima Etapa"
             />
+             <GooglePlacesInput signIn={this.signIn} />
           </View>
         ) : props.tela == 'home' ?
             (
@@ -214,6 +219,63 @@ const LoggedInPage = props => {
       </View>
     </View>
   )
+}
+
+const GooglePlacesInput = () => {
+  return (
+    <GooglePlacesAutocomplete
+      placeholder='Search'
+      minLength={3} // minimum length of text to search
+      autoFocus={false}
+      returnKeyType={'search'} // Can be left out for default return key https://facebook.github.io/react-native/docs/textinput.html#returnkeytype
+      listViewDisplayed='auto'    // true/false/undefined
+      fetchDetails={true}
+      renderDescription={row => row.description} // custom description render
+      onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
+        console.log(data, details);
+      }}
+      
+      getDefaultValue={() => ''}
+      
+      query={{
+        // available options: https://developers.google.com/places/web-service/autocomplete
+        key: 'AIzaSyDKaWvJamhgtX7lJ11QwgOAV7RvOavTKj0',
+        language: 'pt', // language of the results
+        types: '(cities)' // default: 'geocode'
+      }}
+      
+      styles={{
+        textInputContainer: {
+          width: '100%'
+        },
+        description: {
+          fontWeight: 'bold'
+        },
+        predefinedPlacesDescription: {
+          color: '#1faadb'
+        }
+      }}
+      
+      currentLocation={true} // Will add a 'Current location' button at the top of the predefined places list
+      currentLocationLabel="Current location"
+      nearbyPlacesAPI='GooglePlacesSearch' // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
+      GoogleReverseGeocodingQuery={{
+        // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
+      }}
+      GooglePlacesSearchQuery={{
+        // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
+        rankby: 'distance',
+        types: 'food'
+      }}
+
+      filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
+      predefinedPlaces={[homePlace, workPlace]}
+
+      debounce={200} // debounce the requests in ms. Set to 0 to remove debounce. By default 0ms.
+      renderLeftButton={()  => <Image source={require('path/custom/left-icon')} />}
+      renderRightButton={() => <Text>Custom text after the input</Text>}
+    />
+  );
 }
 
 const styles = StyleSheet.create({
